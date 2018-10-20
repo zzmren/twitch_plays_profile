18bf3830399fe44d4cea0f008ab5cb21d61e49fa
Rin-Rin-Hello <katharine.armstrong@hotmail.com>
2018-10-20 13:59:03 -0400

    Add files via upload

---
+++ chatbot/inputProcessing.py
@@ -0,0 +1,27 @@
+def democracy(inputW):
+	#word dictionary is global
+	#convert to all lowercase and see if in array
+	word = inputW.lower()
+	if word in wordDictionary:
+		wordDictionary[word] += 1 
+	else:
+		wordDictionary[word] = 1
+	#now return until next word
+	return
+	
+def mostRepWord():
+	#every 5-10 seconds, most inputted word is the word added to the doc/webpage
+	#now have a count of all repeated words, determine most repeated word
+	#set max to the first word for comparison point
+	keyList = list(wordDictionary.keys())
+	max = keyList[0]
+	for i in keyList:
+		if (wordDictionary[i] > wordDictionary[max]):
+			max = i
+	
+	#now have most repeated word
+	#clear the wordDictionary
+	wordDictionary.clear()
+	
+	return max
+	
